2a2a49eb4f6703db51cc3fd309c321fda81dfccb
Thuancvce150378 <thuancvce150378@fpt.edu.vn>
2023-02-20 15:16:54 +0700

    temp

--- mockup/assets/js/landing.js
+++ mockup/assets/js/landing.js
@@ -1,20 +1,23 @@
 var currentId = null;
 var containerHeight = $(".container").height();
 var heightScroll = $(".container").scrollTop();
-$(".container").on("scroll", function (e) {
+$(document).on("scroll", function (e) {
+  //get offest class container scroll top
   heightScroll = $(this).scrollTop();
-  var index = Math.floor(heightScroll / containerHeight);
-  currentId = $(".panel").eq(index).attr("id");
-  $(".active").removeClass("active");
-  $(".slider li").eq(index).addClass("active");
-  window.location.hash = currentId;
+  console.log(heightScroll);
+  // var index = Math.floor(heightScroll / containerHeight);
+  // currentId = $(".panel").eq(index).attr("id");
+  // $(".active").removeClass("active");
+  // $(".slider li").eq(index).addClass("active");
+  // window.location.hash = currentId;
 });
 // Add smooth scrolling to all links
 $("li").on("click", "a", function (event) {
-  $(".active").removeClass("active");
-  $(this).parent().addClass("active");
   // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
+  console.log(this.hash);
+
+  if (this.hash !== "" && currentId != this.hash) {
+    currentId = this.hash;
     // Prevent default anchor click behavior
     event.preventDefault();
     // Store hash
@@ -22,15 +25,18 @@ $("li").on("click", "a", function (event) {
 
     // Using jQuery's animate() method to add smooth page scroll
     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    console.log($(hash).offset().top);
     $("html, body").animate(
       {
         scrollTop: $(hash).offset().top,
       },
-      100,
+      600,
       function () {
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       }
     );
+    $(".active").removeClass("active");
+    $(this).parent().addClass("active");
   } // End if
 });
