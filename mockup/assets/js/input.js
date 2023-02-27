6bd912da896a1392a943beedbedec6f327a7809f
Thuancvce150378 <thuancvce150378@fpt.edu.vn>
2023-02-28 01:19:43 +0700

    update textarea

--- mockup/assets/js/input.js
+++ mockup/assets/js/input.js
@@ -1,16 +1,25 @@
-$("input").on("blur", function () {
-  if ($(this).val() != "") {
-    $(this).next(".inputSpan").addClass("focus");
-  } else {
-    $(this).next(".inputSpan").removeClass("focus");
-  }
+$("input, textarea")
+  .on("blur", function () {
+    if ($(this).val() != "") {
+      $(this).next(".inputSpan, .areaSpan").addClass("focus");
+    } else {
+      $(this).next(".inputSpan, .areaSpan").removeClass("focus");
+    }
+  })
+  .on("focus", function () {
+    if ($(this).val() != "") {
+      $(this).next(".inputSpan, .areaSpan").addClass("no-animation");
+    } else {
+      $(this).next(".inputSpan, .areaSpan").removeClass("no-animation");
+      $(this).next(".inputSpan, .areaSpan").addClass("focus");
+    }
+  });
+
+$("span.inputSpan, span.areaSpan").on("click", function () {
+  $(this).prev("input, textarea").trigger("focus");
 });
 
-$("input").on("focus", function () {
-  if ($(this).val() != "") {
-    $(this).next(".inputSpan").addClass("no-animation");
-  } else {
-    $(this).next(".inputSpan").removeClass("no-animation");
-    $(this).next(".inputSpan").addClass("focus");
-  }
+$(".inputTxt").on("input", function () {
+  this.style.height = "auto";
+  this.style.height = this.scrollHeight + "px";
 });
