e8aa41beb9c01b56ca78cb780dbedc7dd2079c22
Thuancvce150378 <thuancvce150378@fpt.edu.vn>
2023-03-01 13:18:37 +0700

    fix #1

--- mockup/assets/js/landing.js
+++ mockup/assets/js/landing.js
@@ -4,17 +4,15 @@ var heightScroll = $(".container").scrollTop();
 $(document).on("scroll", function (e) {
   //get offest class container scroll top
   heightScroll = $(this).scrollTop();
-  console.log(heightScroll);
-  // var index = Math.floor(heightScroll / containerHeight);
-  // currentId = $(".panel").eq(index).attr("id");
-  // $(".active").removeClass("active");
-  // $(".slider li").eq(index).addClass("active");
+  var index = Math.floor(heightScroll / containerHeight);
+  currentId = $(".panel").eq(index).attr("id");
+  $(".active").removeClass("active");
+  $(".slider li").eq(index).addClass("active");
   // window.location.hash = currentId;
 });
 // Add smooth scrolling to all links
 $("li").on("click", "a", function (event) {
   // Make sure this.hash has a value before overriding default behavior
-  console.log(this.hash);
 
   if (this.hash !== "" && currentId != this.hash) {
     currentId = this.hash;
@@ -25,7 +23,6 @@ $("li").on("click", "a", function (event) {
 
     // Using jQuery's animate() method to add smooth page scroll
     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    console.log($(hash).offset().top);
     $("html, body").animate(
       {
         scrollTop: $(hash).offset().top,
