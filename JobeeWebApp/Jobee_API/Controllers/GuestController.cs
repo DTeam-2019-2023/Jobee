058e02476dfa7d61e3cd37b673ca649361c92711
thanhtuyenne <88424555+thanhtuyenne@users.noreply.github.com>
2023-03-18 22:52:51 +0700

    upload data to guest

--- JobeeWebApp/Jobee_API/Controllers/GuestController.cs
+++ JobeeWebApp/Jobee_API/Controllers/GuestController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Jobee_API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace Jobee_API.Controllers
 {
@@ -7,5 +10,30 @@ namespace Jobee_API.Controllers
     [ApiController]
     public class GuestController : ControllerBase
     {
+        private readonly Project_JobeeContext _dbContext;
+        private IConfiguration _config;
+        private readonly ILogger<UsersController> _logger;
+        private JwtTokenManager tokenManager;
+        private IDistributedCache _cache;
+        public GuestController(IConfiguration config, Project_JobeeContext dbContext, ILogger<UsersController> logger, IDistributedCache cache)
+        {
+            _config = config;
+            _dbContext = dbContext;
+            _logger = logger;
+            tokenManager = JwtTokenManager.Instance;
+            _cache = cache;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("UserProfile")]
+        public ActionResult<(TbProfile, TbCv, List<Education>)> GetUser(string username)
+        {
+            var account = _dbContext.TbAccounts.FirstOrDefault(i => i.Username == username);
+            var profile = _dbContext.TbProfiles.FirstOrDefault(x => x.Idaccount == account.Id);
+            var cv = _dbContext.TbCvs.FirstOrDefault(i => i.Idaccount == account.Id);
+            var edu = _dbContext.Educations.Where(i => i.Idcv == cv.Id).ToList();
+            return (profile, cv, edu);
+        }
     }
 }
