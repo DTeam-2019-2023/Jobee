3abe7b07916dafea3f289b7c431958becd58f9aa
Thuancvce150378 <thuancvce150378@fpt.edu.vn>
2023-03-21 00:03:27 +0700

    upload error page

--- JobeeWebApp/Jobee/Views/Error/Index.cshtml.cs
+++ JobeeWebApp/Jobee/Views/Error/Index.cshtml.cs
@@ -2,9 +2,15 @@
 
 namespace Jobee.Views.Shared
 {
+    [Route("/Error")]
     public class ErrorController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("{statusCode}")]
+        public IActionResult Index(int statusCode)
         {
             return View();
         }
